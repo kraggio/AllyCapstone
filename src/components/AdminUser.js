7039100faf87733084a1c723347be4604bc0e883
kraggio <keilyraggio18@gmail.com>
2023-04-28 13:38:10 -0400

    trying to square away merges

--- src/components/AdminUser.js
+++ src/components/AdminUser.js
@@ -1,23 +1,34 @@
-import React, {useState} from 'react';
-import App from '../App';
+import React, { useState } from "react";
 
+function Admin() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123",
+  };
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
 
-function Admin(){
-    const adminUser = {
-        email: "admin@admin.com", 
-        password: "admin123"
-    }
-    const [user, setUser] = useState({name: "", email: ""});
-    const[error,setError] = useState("");
+  const Login = (details) => {
+    console.log(details);
+  };
 
-    const Login = details => {
-        console.log(details);
-        }
-
-        const Logout = () => {
-            console.log("Logout");
-        }
-    return 
-
+  const Logout = () => {
+    console.log("Logout");
+  };
+  return (
+    <div className="administrator">
+      {user.email != "" ? (
+        <div className="welcome">
+          <h2>
+            Welcome, <span>{user.email}</span>
+          </h2>
+          <button>Logout</button>
+        </div>
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
+}
 
 export default Admin;
