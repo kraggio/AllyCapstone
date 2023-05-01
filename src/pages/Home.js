c326ee282756d2d86f582f0c0291ba29cb20f215
kraggio <keilyraggio18@gmail.com>
2023-05-01 01:58:56 -0400

    home page edits

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,40 @@
-import React from 'react';
+import React from "react";
 import Slider from "../components/Slider";
+import "../styles/home.css";
 
 function Home() {
-    return (
-        <div className="html">
-        <div className="home">
-          <div className="content">
-            <h3> welcome to Social Spark!</h3>
-            {/* <a href="/partners">
+  return (
+    <div className="html">
+      <div className="home">
+        <div className="content">
+          <h3> welcome to Social Spark!</h3>
+          {/* <a href="/partners">
               <button>Explore!</button>
             </a> */}
-          </div>
-        </div>
-        <Slider />
-        <div className="about">
-          <div className="image">
-            <img src="" alt="viv" />
-          </div>
-          <div class="content">
-            <h3>
-              <span> Meet</span> The Creators.
-            </h3>
-            <p>
-              text about the team....text about the team....text about the team....text about the team....text about the team....text about the team....text about the team....text about the team....text about the team....text about the team....text about the team....text about the team....text about the team....text about the team....text about the team....text about the team....text about the team....text about the team....text about the team....text about the team....
-            </p>
-          </div>
         </div>
       </div>
-    );
-  }
+      <Slider />
+      <div className="about">
+        <div className="image">
+          <img src="" alt="viv" />
+        </div>
+        <div className="content">
+          <h3>
+            <span> Meet</span> The Creators.
+          </h3>
+          <p>
+            text about the team....text about the team....text about the
+            team....text about the team....text about the team....text about the
+            team....text about the team....text about the team....text about the
+            team....text about the team....text about the team....text about the
+            team....text about the team....text about the team....text about the
+            team....text about the team....text about the team....text about the
+            team....text about the team....text about the team....
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Home;
